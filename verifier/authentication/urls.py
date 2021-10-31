72d4f1d70e6a53a7877631ffafa940cbf1405ab9
Anupam <anupam96786@gmail.com>
2021-10-31 07:20:33 +0530

    qr code generator added

--- verifier/authentication/urls.py
+++ verifier/authentication/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path("generate_qr/", views.generate_qr),
+    path("receive_xml/", views.receive_xml),
 ]
--- verifier/authentication/views.py
+++ verifier/authentication/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def generate_qr(request):
+    return render(request, "generate_qr.html")
+
+
+@csrf_exempt
+@api_view(["POST"])
+def receive_xml(request):
+    try:
+        print(request.data["eKycXML"])
+        return Response(status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)

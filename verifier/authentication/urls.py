6d9086b61b7aab15b45a502b446e016445b65dd3
rajarshi07 <rajarshighoshdastidar@gmail.com>
2021-10-31 07:19:34 +0530

    icons and styling

--- verifier/authentication/urls.py
+++ verifier/authentication/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import include,url
 from . import views
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='index'),
+    path('sw.js', TemplateView.as_view(template_name='sw.js', content_type='application/x-javascript')),
+    url(r'^favicon\.ico$',RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
